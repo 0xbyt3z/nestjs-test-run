1d459d18ae3c00587d613fd04e3e7040d454159e
0xbyt3z <rtheekshanafernando@gmail.com>
2023-09-22 16:30:46 +0530

    add interceptors

---
+++ src/interceptors/loggin/loggin.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log(`Intercept Request from ${context.getArgs()[0].url}`);
+    //you can do what ever with the context
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(tap(() => console.log(`Took ${Date.now() - now}ms.`)));
+  }
+}
